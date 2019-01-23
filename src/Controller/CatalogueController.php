e7233612363c9609209d16310a726c4e30efb968
Dmitry <d.i.zykov@gmail.com>
2019-01-24 00:39:46 +0700

    Create framework for shop

---
+++ src/Controller/CatalogueController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CatalogueController extends AbstractController
+{
+    /**
+     * @Route("/catalogue", name="catalogue")
+     */
+    public function index(): Response
+    {
+        return $this->render('catalogue.html.twig');
+    }
+}
