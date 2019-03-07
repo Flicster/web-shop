8d7feed7426b6a059c1343a96af9a00d5dfc0ade
Dmitry <d.i.zykov@gmail.com>
2019-03-07 22:58:36 +0700

    Implement profile orders

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\EditProfileFormType;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,6 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
+    private $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     /**
      * @Route("/profile", name="profile.index")
      */
@@ -30,11 +38,11 @@ class ProfileController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $user->setUpdatedAt(new \DateTime());
-
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
+
+            return $this->redirectToRoute('profile.index');
         }
 
         return $this->render('profile/edit.html.twig', [
@@ -49,8 +57,21 @@ class ProfileController extends AbstractController
     {
         /** @var User $user */
         $user = $this->getUser();
+        $orders = $user->getOrders();
 
-        return $this->render('profile/order.html.twig', [
+        foreach ($orders as $order) {
+            $productsIds = explode(',', $order->getProducts());
+            $productsIdsCount = array_count_values($productsIds);
+            $products = $this->productRepository->findBy(['id' => array_unique($productsIds)]);
+
+            $order->productsEntity = $products;
+            foreach ($products as $product) {
+                $product->count = $productsIdsCount[$product->getId()];
+                $product->sum = $product->getPrice() * $productsIdsCount[$product->getId()];
+            }
+        }
+
+        return $this->render('profile/orders.html.twig', [
             'orders' => $user->getOrders(),
         ]);
     }
